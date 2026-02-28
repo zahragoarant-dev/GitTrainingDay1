b85c7d1398d5103c7adc1e395ecc59de77d5a5a1
Zahra GOARANT <zahra.goarant@gmail.com>
2026-02-28 02:54:10 +0000

    eclipse-workspace/GitTrainingDay1

---
+++ src/com/zaradev/Main.java
@@ -0,0 +1,9 @@
+package com.zaradev;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Version 1");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitTrainingDay1 {
+}
