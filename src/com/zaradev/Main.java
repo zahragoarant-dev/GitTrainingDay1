077456ef85df01e4971da5e2e2168b9b582da461
Zahra GOARANT <zahra.goarant@gmail.com>
2026-02-28 02:56:23 +0000

    Add greet method

--- src/com/zaradev/Main.java
+++ src/com/zaradev/Main.java
@@ -1,7 +1,13 @@
 package com.zaradev;
 
 public class Main {
+	
+	public static void greet() {
+		System.out.println("Bienvenue dans Git !");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Version 2");	}
+		System.out.println("Version 2");	
+		greet();
+	}
 }
