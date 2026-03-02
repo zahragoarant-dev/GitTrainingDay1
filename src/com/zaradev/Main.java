22b0dc3f280740fca48336648fdfd9ad15753518
Zahra GOARANT <zahra.goarant@gmail.com>
2026-03-02 06:30:38 +0000

    :sparkles:feat: add toUpper helper method

--- src/com/zaradev/Main.java
+++ src/com/zaradev/Main.java
@@ -5,9 +5,14 @@ public class Main {
 	public static void greet() {
 		System.out.println("Bienvenue dans Git !");
 	}
+	
+	public static String toUpper(String text) {
+		return text.toUpperCase();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Feature branch version");
 		greet();
+		System.out.println(toUpper("git training"));
 	}
 }
